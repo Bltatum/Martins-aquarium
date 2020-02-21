592963e046569bb1dafc197bac106c7624bf8eb1
Brennen <brennen@Users-MacBook-Pro-3.local>
2020-02-21 15:59:32 -0600

    made changes to locations.css and added event buttons

---
+++ scripts/locations.js
@@ -0,0 +1,38 @@
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener(
+            "click",
+            theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--Cancun").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--Cancun")
+            theDialog.showModal()
+        }
+    )
+
+}
+
+document.querySelector("#button--gallatin").addEventListener(
+    "click",
+    theClickEvent => {
+        const theDialog = document.querySelector("#details--gallatin")
+        theDialog.showModal()
+    }
+)
+
+initializeDetailButtonEvents();
